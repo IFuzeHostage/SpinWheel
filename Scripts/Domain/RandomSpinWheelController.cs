272a4ae174d344c17cf5e503e807dbbc7c9c4556
IFuzeHostage <alexander.hamst@gmail.com>
2024-07-06 14:50:31 +0300

    Add ISpinWheelController

---
+++ Scripts/Domain/RandomSpinWheelController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IFuzeHostage.SpinWheel.Data;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace IFuzeHostage.SpinWheel
+{
+    internal class RandomSpinWheelController : ISpinWheelController
+    {
+        private List<RewardData> _rewardDatas;
+        
+        public RandomSpinWheelController(List<RewardData> rewardDatas)
+        {
+            _rewardDatas = rewardDatas;
+        }
+        
+        public void Init()
+        {
+            
+        }
+
+        public List<RewardData> GetRewardList()
+        {
+            return _rewardDatas;
+        }
+
+        public Task<RewardData> GetRandomReward()
+        {
+            if (_rewardDatas.Count <= 0)
+            {
+                return Task.FromException<RewardData>(new IndexOutOfRangeException("SpinWheel: Reward list is empty"));
+            }
+            
+            int randomIndex = Random.Range(0, _rewardDatas.Count);
+            RewardData selectedData = _rewardDatas[randomIndex];
+
+            return Task.FromResult(selectedData);
+        }
+    }
+}
